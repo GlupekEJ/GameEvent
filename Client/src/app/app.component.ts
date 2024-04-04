f4e9d299e6965752b81f270ff2fd3a4fd7fbb9b9
GlupekEJ <r1001285@ucll.be>
2024-04-04 18:05:22 +0200

    footer aangemaakt met al een klein beetje css

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderNavComponent],
+  imports: [RouterOutlet, HeaderNavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
