42d786ec5bbab693b320069406741025c6ca325f
GlupekEJ <r1001285@ucll.be>
2024-05-19 23:52:18 +0200

    reserveer naar registreer changed en aan het testen met radio selection van Angular material

--- Client/src/app/formulier-standhouders/formulier-standhouders.component.ts
+++ Client/src/app/formulier-standhouders/formulier-standhouders.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-formulier-standhouders',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatRadioModule],
   templateUrl: './formulier-standhouders.component.html',
   styleUrl: './formulier-standhouders.component.css',
 })
-export class FormulierStandhoudersComponent {}
+export class FormulierStandhoudersComponent {
+  categorySelection!: string;
+  categories: string[] = ['All', 'Retro', 'New', 'Merch'];
+}
