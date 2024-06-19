c9f38f261263d7a2b76945e4f48e7c50397defd7
LarsG <lars.goedhuys@student.ucll.be>
2024-06-19 15:28:28 +0200

    Update van reservatiepagina (1 form element toegepast, alle inputvelden materials gemaakt en een validator op geplaatst) + Update van betalingspagina (validator op radiovelden geplaatst).

--- Client/src/app/betaalpagina/betaalpagina.component.ts
+++ Client/src/app/betaalpagina/betaalpagina.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { RouterLink } from '@angular/router';
+
+interface BetalingMethodes {
+  betalingsMethode: string;
+}
+
 
 @Component({
   selector: 'app-betaalpagina',
   standalone: true,
-  imports: [FormsModule, MatRadioModule, RouterLink],
+  imports: [
+    FormsModule,
+    MatRadioModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './betaalpagina.component.html',
   styleUrl: './betaalpagina.component.css',
 })
 export class BetaalpaginaComponent {
-  paymentOptions!: string;
-  paymentOption: string[] = ['Bancontact', 'Mastercard', 'VISA', 'PayPal'];
+  methodeControl = new FormControl<BetalingMethodes | null>(null, [
+    Validators.required,
+  ]);
+  methodes: BetalingMethodes[] = [
+    { betalingsMethode: 'Bancontact' },
+    { betalingsMethode: 'Mastercard' },
+    { betalingsMethode: 'VISA' },
+    { betalingsMethode: 'PayPal' }
+  ];
 }
